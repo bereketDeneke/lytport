16985127d4b3e9f46a361b85391ecc63f3177e80
bereketDeneke <bereketsiraw123@gmail.com>
2024-10-09 22:08:15 -0400

    ER diagram

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
 from database import Database
 
 db = Database()
+
+#todo: we create all our database here
+
+#todo: public figures info table
+
+#todo: post table
+
+#todo: comments
+
+
 db.create_table('user', {'name':"VARCHAR(100) NOT NULL", 'followers':"INT"})
 db.insert('user', ['name', 'followers'], ['John Doe', 30])
 db.update('user', ['name'], ['Jane Doe'], 'id', 1)
