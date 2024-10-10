5f8a9f64c6dda88edc72f368a86dae6c8b6f0950
bereketDeneke <bereketsiraw123@gmail.com>
2024-10-10 11:04:24 -0400

    dummy data added by Moudja

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ from database import Database
 db = Database()
 
 #todo: we intitialize our tables here
-
 db.create_table('user', {'name':"VARCHAR(100) NOT NULL", 'followers':"INT"})
 db.insert('user', ['name', 'followers'], ['John Doe', 30])
 db.update('user', ['name'], ['Jane Doe'], 'id', 1)
