f4d68d248254ba82b2413199e6c5cd9fd52678a7
bereketDeneke <bereketsiraw123@gmail.com>
2024-10-10 13:03:46 -0400

    minor database error handling fix

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ db = Database()
 #todo: we intitialize our tables here
 db.create_table('user', {'name':"VARCHAR(100) NOT NULL", 'followers':"INT"})
 db.insert('user', ['name', 'followers'], ['John Doe', 30])
-db.update('user', ['name'], ['Jane Doe'], 'id', 1)
+db.update('user', ['name'], ['Alice Doe'], 'id', 1)
 db.delete('user', 'id', 1)
 
 rows = db.select('user', ['name', 'followers'])
